3a7a21aa9e549b5630a50eb9dd292bd42e0c0711
ProgOleg <45142677+ProgOleg@users.noreply.github.com>
2020-04-23 23:43:04 +0300

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '-_=$q$f=#s$zqhs_1-aji_l$+*qu=edxnnqca(7(i3q8f0&v@6'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.0.104','localhost']
+ALLOWED_HOSTS = ['35.247.104.185','192.168.0.104','localhost']
 
 
 # Application definition
