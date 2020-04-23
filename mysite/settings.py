357ad04d04ed3e3fb2b5e59597776d574d77d1ba
ProgOleg <45142677+ProgOleg@users.noreply.github.com>
2020-04-23 23:49:47 +0300

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -80,8 +80,8 @@ DATABASES = {
         'NAME': 'new_db',
         'USER': 'oleg',
         'PASSWORD': '12a378d',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
+        'HOST': '',
+        'PORT': '',
     }
 }
 
